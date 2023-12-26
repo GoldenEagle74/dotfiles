17ef7a9bc2fe057591bf0fb3237231f8f9fc0f55
GoldenEagle74 <mshchannikov@yandex.ru>
2023-12-27 02:40:58 +0300

    added hypr dotfiles

---
+++ hypr/scripts/brightness_control.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+brightness_step=5
+notification_timeout=1000
+
+# Функция для получения текущего уровня яркости
+get_brightness() {
+    light -G | awk '{print int($1)}'
+}
+
+# Функция для отправки уведомления о яркости
+send_brightness_notification() {
+    current_brightness=$(get_brightness)
+    dunstify -a "changeBrightness" -u low -h string:x-dunst-stack-tag:brightness " Brightness: ${current_brightness}%"
+}
+
+# Обработка параметров
+while getopts ":di" opt; do
+    case $opt in
+        d)
+            light -U $brightness_step
+            send_brightness_notification
+            ;;
+        i)
+            light -A $brightness_step
+            send_brightness_notification
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
---
+++ hypr/scripts/changevolume.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# Путь к иконкам
+msgTag=volume
+volume_step=5
+notification_timeout=1000
+
+# Функция для получения имени микрофона
+get_microphone_name() {
+    source_list=$(pamixer --list-sources)
+    source_name=$(echo "$source_list" | grep -oP '(?<=\d ")(alsa_input[^"]+)' | head -n 1)
+    echo "$source_name"
+}
+
+# Функция для отправки уведомления о громкости
+send_volume_notification() {
+    current_volume=$(pamixer --get-volume-human | sed 's/[^0-9]*//g')
+    mute_status=$(pamixer --get-mute)
+
+    if [[ $mute_status == "true" ]]; then
+        dunstify -a "changeVolume" -u low -h string:x-dunst-stack-tag:$msgTag " Volume muted"
+    else
+        if [ "$current_volume" -lt 50 ]; then
+            volume_icon=""
+        else
+            volume_icon=""
+        fi
+
+        dunstify -a "changeVolume" -u low -h string:x-dunst-stack-tag:$msgTag \
+        -h int:value:"$current_volume" -t $notification_timeout "$volume_icon Volume: ${current_volume}%"
+    fi
+}
+
+# Функция для отправки уведомления о муте микрофона
+send_mute_microphone_notification() {
+    microphone_name=$(get_microphone_name)
+    mute_status=$(pamixer --source "$microphone_name" --get-mute)
+
+    if [[ $mute_status == "true" ]]; then
+        dunstify -a "changeVolume" -u low -h string:x-dunst-stack-tag:$msgTag " Microphone muted"
+    else
+        dunstify -a "changeVolume" -u low -h string:x-dunst-stack-tag:$msgTag " Microphone unmuted"
+    fi
+}
+
+# Обработка параметров
+while getopts ":o:i:" opt; do
+    case $opt in
+        o)
+            case $OPTARG in
+                m)
+                    pamixer --toggle-mute
+                    send_volume_notification
+                    ;;
+                d)
+                    pamixer --decrease $volume_step
+                    send_volume_notification
+                    ;;
+                i)
+                    pamixer --increase $volume_step
+                    send_volume_notification
+                    ;;
+                *)
+                    echo "Invalid option argument for -o"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        i)
+            case $OPTARG in
+                m)
+                    microphone_name=$(get_microphone_name)
+                    pamixer --source "$microphone_name" --toggle-mute
+                    send_mute_microphone_notification
+                    ;;
+                *)
+                    echo "Invalid option argument for -i"
+                    exit 1
+                    ;;
+            esac
+            ;;
+        \?)
+            echo "Invalid option: -$OPTARG"
+            exit 1
+            ;;
+        :)
+            echo "Option -$OPTARG requires an argument."
+            exit 1
+            ;;
+    esac
+done
